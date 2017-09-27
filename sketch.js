ec4ca96c56b42e2b97acefc5c5d94d169216bc52
maojh <mjh.ilbasco@gmail.com>
2017-09-27 13:54:42 +0200

    first edit setch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
 function setup() {
-
+  createCanvas(window.width, window.height);
+  background(0);
 }
 
 function draw() {
   
+  translate(width/2, height/2);
+  noStroke();
+  fill(240);
+  ellipse(mouseX, mouseY, width/10, height/2);
 }
