6c02eb6ca8ea16a96aa86fd0df69ca5a1893feac
maojh <mjh.ilbasco@gmail.com>
2017-10-03 12:09:33 +0200

    all shapes in place, still w.i.p.

--- #1_Tachikoma/sketch.js
+++ #1_Tachikoma/sketch.js
@@ -27,7 +27,6 @@ function draw() {
 
   //testa
   testa(250,250);
-
   //addome
   addome(400, 180);
   /*arti:   1D || 1S
@@ -36,68 +35,86 @@ function draw() {
          3D _/    |_ 3S
   */
   //1D - 1S
-  art3(250,280);
-  //2D - 2S
-  art2(350,280);
-  //3D - 3S
   art1(190, 250, PI/3);
+  //2D - 2S
+  art2(230,280);
+  //3D - 3S
+  art3(350,280);
   //
 }
 
 function testa(posx, posy) {
   push();
-  translate(posx, posy);
-  fill(0, 0, 150, 200);
-  //testa
-  ellipse(0, 0, 150, 100);
-  //occhi
-  fill(250, 150);
-  ellipse(-60, -20, eyeSize*1.1, eyeSize);
-  ellipse(-5, -20, eyeSize*1.3, eyeSize*1.2);
-  //antenna
-  fill(0, 0, 150, 200);
-    push();
-      translate(-10, -45);
-      rotate(-PI/2);
-      rect( 0, 0, 30, 10)
-      translate(32, 0);
-      rect( 0, 0, 20, 10)
+      translate(posx, posy);
+      fill(0, 0, 150, 200);
+      //testa
+      ellipse(0, 0, 150, 100);
+      //occhi
+      fill(250, 150);
+      ellipse(-60, -20, eyeSize*1.1, eyeSize);
+      ellipse(-5, -20, eyeSize*1.3, eyeSize*1.2);
+      //antenna
+      fill(0, 0, 150, 200);
+      push();
+        translate(-10, -45);
+        rotate(-PI/2);
+        rect( 0, 0, 30, 10)
+        translate(32, 0);
+        rect( 0, 0, 20, 10)
+      pop();
     pop();
-  pop();
 }
 
 function addome(posx, posy) {
   push();
-  translate(posx, posy);
+    translate(posx, posy);
 
-  fill(0, 0, 150, 200);
-  ellipse(0, 0, 200, 250);
+    fill(0, 0, 150, 200);
+    rect(-100, -125, 200, 250);
   pop();
 }
 
 function art1(posx, posy, angle) {
+  var lenBra = lenLeg/2;
+  var largBra = largLeg*0.6;
   push();
-  translate(posx, posy);
-  fill(0, 0, 150, 200);
-  rotate(angle);
-  rect(0, 0, largLeg*0.6, lenLeg/2);
+    translate(posx, posy);
+    fill(0, 0, 150, 200);
+    push();
+      rotate(angle);
+      rect(0, 0, largBra, lenBra);
+    pop();
+    //chele
+    push();
+      fill('grey');
+      translate(-lenBra);
+      rotate(PI/3);
+      rect(0,0,20,7);
+    pop();
   pop();
 }
 
 function art2(posx, posy) {
   push();
-  translate(posx, posy);
+    translate(posx, posy);
 
-  fill(0, 0, 150, 200);
-  rect(0, 0, largLeg, lenLeg);
+    fill(0, 0, 150, 200);
+    //arto
+    rect(0, 0, largLeg, lenLeg);
+    //rotella
+    fill('grey');
+    ellipse(20, 150, 40, 40);
   pop();
 }
 
 function art3(posx, posy) {
   push();
-  translate(posx, posy);
+    translate(posx, posy);
 
-  fill(0, 0, 150, 200);
-  rect(0, 0, largLeg, lenLeg);
+    fill(0, 0, 150, 200);
+    rect(0, 0, largLeg, lenLeg);
+    //piedi
+    fill('grey');
+    rect(-15, 140, 65, 30);
   pop();
 }
