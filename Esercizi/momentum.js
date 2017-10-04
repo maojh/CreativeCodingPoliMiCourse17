5773a752c3c6388352bd0a071b3ab40e1e9268f4
mjh <mjh.ilbasco@gmail.com>
2017-10-04 11:48:02 +0200

    update

---
+++ Esercizi/momentum.js
@@ -0,0 +1,16 @@
+
+//Momentum wheel
+
+function setup() {
+  createCanvas(800,600);
+  noFill();
+  stroke(0);
+  background(230);
+}
+
+function draw() {
+
+  translate(width/2, height/2);
+
+  ellipse(0, 0, 100, 100);
+}
