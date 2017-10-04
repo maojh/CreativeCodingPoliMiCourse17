84f0c684c30fe6b3d3f60733e4556a9ee603d381
maojh <mjh.ilbasco@gmail.com>
2017-10-04 10:43:37 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,8 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  console.log(windowWidth, windowHeight);
-  background(0);
+
 }
 
 
 function draw() {
-  noStroke();
-  fill(2, 35);
-  rect(0,0, windowWidth, windowHeight);
-  fill(240);
-  ellipse(mouseX, mouseY, windowWidth/10, windowWidth/10);
+
 }
