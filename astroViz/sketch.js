8cce1fdb7326c174c163992c4288db9c0e38f380
Unknown <mjh.ilbasco@gmail.com>
2017-11-14 00:19:41 +0100

    readMe, title, and object setup

--- astroViz/sketch.js
+++ astroViz/sketch.js
@@ -1,5 +1,6 @@
-var myData;
+var myData, iss;
 var people = [];
+var mw, mh;
 
 function preload() {
   myData = loadJSON('assets/peopleinspace.json');
@@ -7,75 +8,77 @@ function preload() {
 
 function setup() {
   createCanvas(500, 500);
-  
+
   //print(myData);
   for(var i = 0; i < myData.people.length; i++) {
     var astroData = myData.people[i];
-    print(astroData);
-    var newAstronaut = new Astronaut(astroData.launchdate, astroData.name, astroData.title);
+    var newAstronaut = new Astronaut(astroData.launchdate, astroData.name, astroData.title, astroData.country);
     people.push(newAstronaut);
   }
+
+  iss = new station(people);
+
+  mw = width/20;
+  mh = height/20;
 }
 
 function draw() {
-  background(240);
-  
-	for(var i = 0; i < people.length; i++) {
-	  var astronaut = people[i];
-	  astronaut.move();
-	  astronaut.display();
-	}
+  background(40);
+  translate(width/2, height/2);
 
+  iss.display();
 }
 
-function Astronaut(launchDate, name, title) {
-    
+function station(people) {
+  this.incrementX = 1;
+  this.pox = height/2;
+
+  this.display = function() {
+    push();
+      if(this.incrementX > width-mw || this.incrementX < mw) {
+        this.incrementX = 1 - this.incrementX;
+      } else {
+        print(iss.pox)
+        translate(this.pox, 0);
+        this.pox += this.incrementX;
+      }
+
+      for(var i = 0; i < people.length; i++) {
+        people[i].display(i);
+      }
+    pop();
+  }
+}
+
+function Astronaut(launchDate, name, title, poy, pox) {
+
     this.name = name;
     this.title = title;
-    
+    this.x = pox;
+    this.y = poy;
+
     // transform the launch date from String
     // to a date Object calculated in milliseconds
     this.launchDate = Date.parse(launchDate);
     // calculate the time spent in space
-    var timeInSpace = Date.now() - this.launchDate;
-    // define radius according to the time spent in space
-    this.radius = floor(timeInSpace / (1000 * 60 * 60 * 24)) / 5;
-    
-    this.x = random(this.radius, width-this.radius);
-    this.y = random(this.radius, height-this.radius);
-    
-    this.incrementX = 1;
-    this.incrementY = 1;
-    
-    this.display = function() {
-        
+    this.timeInSpace = Date.now() - this.launchDate;
+
+    this.display = function(i) {
+        var xpos = i*20;
+        print(xpos);
         if(this.title == 'commander') {
-          fill(255,0,0);
+          fill(50,50,250);
         } else {
-          fill(255);
-        }
-        ellipse(this.x, this.y, this.radius * 2);
-        
-        fill(0);
-        textAlign(CENTER);
-        text(this.name, this.x, this.y + this.radius + 15);
-    }
-    
-    this.move = function() {
-        
-        this.x += this.incrementX;
-        this.y += this.incrementY;
-        
-        if (this.x > width - this.radius || this.x < this.radius){
-            this.incrementX *= -1
-            print(this.x);
-            print(this.radius);
+          fill(200,250,200);
         }
 
-        if (this.y > height - this.radius || this.y < this.radius){
-            this.incrementY *= -1
-            print(this.y);
-            print(this.radius);
-        }
-    }   
+        ellipse( xpos, 0, 20);
+
+        push();
+          fill(0);
+          rotate(HALF_PI);
+          textAlign(CENTER);
+          text(this.name, xpos, 0);
+        pop();
+    }
 }
