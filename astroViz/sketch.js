e2371a201d0b78ea7a40816f2b74819ca422b2b2
Unknown <mjh.ilbasco@gmail.com>
2017-11-14 21:10:13 +0100

    first version

--- astroViz/sketch.js
+++ astroViz/sketch.js
@@ -24,7 +24,7 @@ function setup() {
 
   for(var i = 0; i < myData.people.length; i++) {
     var astroData = myData.people[i];
-    var newAstronaut = new Astronaut(i, astroData.launchdate, astroData.biophoto, astroData.name, astroData.title, astroData.country);
+    var newAstronaut = new Astronaut(i, astroData.launchdate, astroData.biophoto, astroData.name, astroData.title, astroData.country,astroData.careerdays);
     people.push(newAstronaut);
   }
 
@@ -51,21 +51,20 @@ function draw() {
 
 
   push();
-  var tras = -width/40+width/3.5*sin(c/10);
-    translate(tras,0);
+    var tras = -width/40+width/2.5*sin(c/10);
+    //translate(tras,0);
     var scal = map(cos(c/10), -1, 1, 0.5, 1);
     scale(scal);
 
-    // translate(width/2-130*sin(frameCount/100), height/2-20);
-    // scale(1+cos(frameCount/100)*0.8);
     if(scal>0.7) {
       push();
-        rotate(c/150);
-        image(isss, 0, 0, height/8, height/8);
+        // rotate(c/150);
+        image(isss, tras, 0, height/8, height/8);
       pop();
       for(var p=0; p<people.length; p++) {
-        people[p].display(tras);
+          people[p].display(tras);
       }
+
     }
   pop();
 
@@ -73,11 +72,11 @@ function draw() {
    //noLoop();
 }
 
-function Astronaut(i, launchDate, biophoto, name, title, country) {
+function Astronaut(i, launchDate, biophoto, name, title, country, careerdays) {
     this.name = name;
     this.title = title;
-    this.radius = 15;
-    this.x = i*this.radius*1.5;
+    this.radius = width/40;
+    this.x = i*this.radius*1;
     this.y = height/2;
     this.biophoto = biophoto;
 
@@ -91,20 +90,46 @@ function Astronaut(i, launchDate, biophoto, name, title, country) {
       if(mouseX>0&&mouseX<width && mouseY>0&&mouseY<height) {
         var mX = mouseX-posx*2;
         var mY = mouseY;
-        console.log(posx+people[0].x + " " + mX);
-        //console.log(dist(mX, mY, this.x, this.y) );
+        // console.log(dist(mouseX, mouseY, this.x, this.y)/30 +" "+ this.radius*0.75);
       if(dist(mouseX, mouseY, this.x, this.y) < this.radius*0.75) {
-          fill(0, 50, 250);
-      } else {
-          fill(255);
+          //fill(0, 50, 250, 255);
       }
     }
   }
 
-    this.display = function(posx) {
+
+  this.display = function(posx) {
+    noStroke();
+    var opac = map(careerdays, 0, 175, 100, 255);
+
+    switch(country) {
+      case 'usa':
+        fill("Crimson");
+      break;
+      case 'russia':
+        fill("Cyan");
+      break;
+      case 'italy':
+        fill("SpringGreen");
+      break;
+    }
       this.hover(posx);
-      fill(random(100, 255),random(100, 255),random(100, 255));
-      ellipse(i*25,this.radius*2,20,20);
+      var h = i;
+      posx += this.radius*h;
+
+      var posy;
+      if (i%2==0) {
+        posy = this.radius*2;
+      } else {
+        posy = this.radius*4;
+        posx -= this.radius;
+      }
+      if(this.title=="Commander") {
+        rectMode(CENTER);
+        rect(posx,posy,this.radius, this.radius);
+      } else {
+        ellipse(posx,posy,this.radius, this.radius);
+      }
 
     }
 }
@@ -113,6 +138,7 @@ function Astronaut(i, launchDate, biophoto, name, title, country) {
 function mousePressed() {
     inc = abs(1-inc);
   console.log(inc);
+  save();
 }
 
 function windowResized() {
